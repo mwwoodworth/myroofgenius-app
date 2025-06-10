0a6525bee200c3552f7dfe3172aa7792d1b9c05d
mwwoodworth <47643255+mwwoodworth@users.noreply.github.com>
2025-06-10 09:09:37 -0700

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>MyRoofGenius</h1>
+      <p>Welcome! This is a placeholder React application.</p>
+    </div>
+  );
+}
+
+export default App;
