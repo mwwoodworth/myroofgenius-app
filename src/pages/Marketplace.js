bd1cdb5ef60cc0af3ac7628eca81baecffb70b9e
mwwoodworth <47643255+mwwoodworth@users.noreply.github.com>
2025-06-16 10:31:50 -0700

    Create Marketplace.js

---
+++ src/pages/Marketplace.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Marketplace = () => (
+  <div style={{ padding: '2rem' }}>
+    <h1>Marketplace</h1>
+    <p>Product grid coming soon…</p>
+  </div>
+);
+
+export default Marketplace;
