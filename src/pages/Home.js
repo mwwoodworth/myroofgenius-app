dc1d0dc6a3f772bb0dbb3bb7ddf377f59ba0e5c7
mwwoodworth <47643255+mwwoodworth@users.noreply.github.com>
2025-06-16 10:29:43 -0700

    Create Home.js

---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Home = () => (
+  <div style={{ padding: '2rem' }}>
+    <h1>Welcome to MyRoofGenius</h1>
+    <p>Your one‑stop marketplace for roofing knowledge & tools.</p>
+  </div>
+);
+
+export default Home;
