007d09d369ecb5b2d7a8fd898657cb58192a1a1d
mwwoodworth <47643255+mwwoodworth@users.noreply.github.com>
2025-06-16 10:34:55 -0700

    Create Account.js

---
+++ src/pages/Account.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const Account = () => (
+  <div style={{ padding: '2rem' }}>
+    <h1>Your Account</h1>
+    <p>Login, orders, and downloads coming soon…</p>
+  </div>
+);
+
+export default Account;
