14443b5bc43b407726f0ac254fc04925b3bb91fd
mwwoodworth <47643255+mwwoodworth@users.noreply.github.com>
2025-06-16 15:40:41 -0700

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{js,jsx,ts,tsx,html}"],
-    theme: {
-      extend: {},
+  content: ["./src/**/*.{js,jsx,ts,tsx,html}"],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1e40af", // brand blue
+        secondary: "#f97316", // brand orange
+      },
     },
-    plugins: [],
-  };
-  
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    themes: [
+      {
+        roofgenius: {
+          primary: "#1e40af",
+          secondary: "#f97316",
+          accent: "#37cdbe",
+          neutral: "#3d4451",
+          "base-100": "#ffffff",
+        },
+      },
+    ],
+  },
+};
