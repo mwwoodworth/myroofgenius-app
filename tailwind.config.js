4bae24acd4b199a533c37923571da3ce02ddc0f1
mwwoodworth <47643255+mwwoodworth@users.noreply.github.com>
2025-06-20 16:20:25 -0600

    chore: include root dirs in tailwind scan

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{js,jsx,ts,tsx,html}"],
+    content: [
+        "./src/**/*.{js,jsx,ts,tsx,html}",
+        "./components/**/*.{js,jsx,ts,tsx,html}",
+        "./app/**/*.{js,jsx,ts,tsx,html}",
+        "./theme/**/*.{js,jsx,ts,tsx,html}"
+    ],
     theme: {
       extend: {},
     },
