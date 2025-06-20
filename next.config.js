b6a35f43e4e24a9aedc6e604f42fc02b69c7da5a
mwwoodworth <47643255+mwwoodworth@users.noreply.github.com>
2025-06-20 16:14:53 -0600

    chore: add tsconfig and alias

--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
+
 const nextConfig = {
   reactStrictMode: true,
   experimental: {
     appDir: true,
+  },
+  webpack(config) {
+    config.resolve.alias['#components'] = path.join(__dirname, 'components');
+    config.resolve.alias['#data'] = path.join(__dirname, 'data');
+    config.resolve.alias['#theme'] = path.join(__dirname, 'theme');
+    return config;
   },
 };
 
