7d50a563de85e5b35c864a2f740439db29ee4ac0
mwwoodworth <47643255+mwwoodworth@users.noreply.github.com>
2025-06-23 12:25:33 -0700

    Update supabaseClient.js

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -3,5 +3,5 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
 const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
-export const supabase = createClient(supabaseUrl, supabaseAnon
- export default supabase;                                    Key);
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
+ export default supabase;
