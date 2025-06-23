749910a1994b4f6b6365a3fa44cd65f518b13143
mwwoodworth <47643255+mwwoodworth@users.noreply.github.com>
2025-06-23 12:13:07 -0700

    fix: remove invalid theme import and unblock build

--- src/app/provider.tsx
+++ src/app/provider.tsx
@@ -3,11 +3,11 @@
 import { AuthProvider } from '@saas-ui/auth'
 import { SaasProvider } from '@saas-ui/react'
 
-import { theme } from '#theme'
+
 
 export function Provider(props: { children: React.ReactNode }) {
   return (
-    <SaasProvider theme={theme}>
+    <SaasProvider>
       <AuthProvider>{props.children}</AuthProvider>
     </SaasProvider>
   )
