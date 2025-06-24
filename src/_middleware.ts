f1440214b6b3b99d3115f300d797b0e04e861678
mwwoodworth <47643255+mwwoodworth@users.noreply.github.com>
2025-06-24 14:37:10 -0700

    Update _middleware.ts

--- src/_middleware.ts
+++ src/_middleware.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server'
-_imort type { NextRequest } from 'next/server'
+import type { NextRequest } from 'next/server'
 
 export function middleware(request: NextRequest) {
   const response = NextResponse.next()
